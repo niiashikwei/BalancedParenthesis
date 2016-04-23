8cc281955dada37d0a06225f0fdcc7c5fc5cebd0
niiashikwei <niiashikwei@gmail.com>
2016-04-23 02:26:05 -0700

    Nii - added last bit of test coverage, i think

--- test/ParenthesisConfigurationTest.java
+++ test/ParenthesisConfigurationTest.java
@@ -11,9 +11,23 @@ public class ParenthesisConfigurationTest{
     }
 
     @Test
-    public void shouldParenthesisIfValid() throws Exception {
+    public void shouldReturnParenthesisIfParenthesis() throws Exception {
         ParenthesisConfiguration parenthesisConfiguration = new ParenthesisConfiguration();
         Parenthesis parenthesisForChar = parenthesisConfiguration.getParenthesisForChar('(');
         assertThat(parenthesisForChar, is(new Parenthesis('(', ')')));
+    }
+
+    @Test
+    public void shouldParenthesisIfBracket() throws Exception {
+        ParenthesisConfiguration parenthesisConfiguration = new ParenthesisConfiguration();
+        Parenthesis parenthesisForChar = parenthesisConfiguration.getParenthesisForChar(']');
+        assertThat(parenthesisForChar, is(new Parenthesis('[', ']')));
+    }
+
+    @Test
+    public void shouldParenthesisIfCurlyBrace() throws Exception {
+        ParenthesisConfiguration parenthesisConfiguration = new ParenthesisConfiguration();
+        Parenthesis parenthesisForChar = parenthesisConfiguration.getParenthesisForChar('{');
+        assertThat(parenthesisForChar, is(new Parenthesis('{', '}')));
     }
 }
