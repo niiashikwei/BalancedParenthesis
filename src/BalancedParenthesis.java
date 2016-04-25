dfa1025aae3903a5cab37203d47690022ccf38ec
niiashikwei <niiashikwei@gmail.com>
2016-04-25 14:00:25 -0400

    Nii - completed main program

--- src/BalancedParenthesis.java
+++ src/BalancedParenthesis.java
@@ -1,6 +1,13 @@
 
 public class BalancedParenthesis {
-    public static void main(String args[]){
+    public static void main(String[] input){
         Checker checker = new Checker(new DefaultParenthesisConfiguration());
+        String inputString = input[0];
+        boolean areBalanced = checker.checkBalanced(inputString);
+        if (areBalanced){
+            System.out.println("For input '" + inputString + "' parenthesis are balanced.");
+        }else{
+            System.out.println("For input '" + inputString + "' parenthesis are not balanced.");
+        }
     }
 }
